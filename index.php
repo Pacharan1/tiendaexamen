26911fadce5b030c1c4308031a6626bd2c09df91
Pacharan <emiliodelacal@hotmail.com>
2023-12-17 17:44:35 +0100

    inicio del proyecto

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tienda Apple</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <table>
+        <tr class="encabezado">
+            <td>ID</td>
+            <td>Nombre del Producto</td>
+            <td>Precio</td>
+            <td></td>
+        </tr>
+
+        <tr>
+            <td>1</td>
+            <td>Producto A</td>
+            <td>$20.00</td>
+            <td>
+                <button class="btn-modificar">Modificar</button>
+                <button class="btn-borrar">Borrar</button>
+            </td>
+        </tr>
+        <!-- Agrega más filas según sea necesario -->
+
+    </table>
+
+</body>
+
+</html>
